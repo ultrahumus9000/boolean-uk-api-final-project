a41291abaaacbb41e7f55597b8d3bb5949087bf4
Linlin Li <paris1900lll@gmail.com>
2021-08-18 09:20:43 +0100

    edited post

--- frontend/food-front-end/src/components/NewPostForm.tsx
+++ frontend/food-front-end/src/components/NewPostForm.tsx
@@ -5,15 +5,18 @@ import "../styles/new_post_form.css";
 
 function NewPostForm() {
   const createPost = useStore((store) => store.createPost);
-
+  const activeUser = useStore((store) => store.activeUser);
   function handleSubmit(event: SyntheticEvent) {
     event.preventDefault();
     const targetEvent = event.target as HTMLFormElement;
 
     let newPost = {
+      date: new Date().toISOString().slice(0, 10),
+      likes: 0,
       picture: targetEvent.picture.value,
       text_content: targetEvent.text_content.value,
       address: targetEvent.address.value,
+      userId: activeUser,
     };
     createPost(newPost);
     targetEvent.reset();
