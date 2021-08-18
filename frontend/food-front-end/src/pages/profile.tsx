ccca6a9a5ea3a38e19f4b5fbf964afcdbf5e6215
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-08-18 21:15:34 +0100

    completed profile page

--- frontend/food-front-end/src/pages/profile.tsx
+++ frontend/food-front-end/src/pages/profile.tsx
@@ -4,35 +4,38 @@ import "../styles/profile.css";
 import useStore from "../store";
 
 function Profile() {
-  const user = useStore((store) => store.activeUser);
-
-
   return (
     <main className="profile-page">
-      <h2 className="edit-profile-title">Edit your profile:</h2>
-      <form className="edit-profile-form">
-        <label className="edit-profile-label">
-          Avatar:
-          <input type="text" name="avatar" placeholder="" />
-        </label>
-        <label className="edit-profile-label">
-          Username:
-          <input type="text" name="username" placeholder="clair456" />
-        </label>
-        <label className="edit-profile-label">
-          First name:
-          <input type="text" name="firstName" placeholder="Clarie" />
-        </label>
-        <label className="edit-profile-label">
-          Last name:
-          <input type="text" name="lastName" placeholder="Smith" />
-        </label>
-        <label className="edit-profile-label">
-          Email:
-          <input type="email" name="email" placeholder="clarie@email.com" />
-        </label>
+      <div className="profile-to-edit">
+        <h2 className="edit-profile-title">Your profile:</h2>
+        <div className="edit-avatar">
+          <img
+            className="avatar"
+            src="https://www.adorama.com/alc/wp-content/uploads/2017/06/1-shutterstock_588634790.jpg"
+            alt="avatar"
+          ></img>
+          <button className="edit-profile">Edit</button>
+        </div>
+        <div className="edit-element">
+          <p>Username</p>
+          <button className="edit-profile">Edit</button>
+        </div>
+        <div className="edit-element">
+          <p>firstName</p>
+          <button className="edit-profile">Edit</button>
+        </div>
+
+        <div className="edit-element">
+          <p>lastName</p>
+          <button className="edit-profile">Edit</button>
+        </div>
+
+        <div className="edit-element">
+          <p>email</p>
+          <button className="edit-profile">Edit</button>
+        </div>
         <button className="edit-profile-submit">Submit</button>
-      </form>
+      </div>
     </main>
   );
 }
