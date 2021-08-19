5ab5ed33d1ac8e928f5a985a18ab3debd91217a9
Linlin Li <paris1900lll@gmail.com>
2021-08-19 20:55:19 +0100

    css

--- frontend/food-front-end/src/components/Post.tsx
+++ frontend/food-front-end/src/components/Post.tsx
@@ -43,8 +43,70 @@ function Post({ post, users, display, handleDisplay }: PostProps) {
       {display ? (
         <>
           <div className="post_info">
-            <p>{post.text_content}</p>
-            <p>{post.address}</p>
+            <p>
+              <svg
+                width="35px"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 32 32"
+              >
+                <title>BUSINESS</title>
+                <g id="_23.SPEAK" data-name="23.SPEAK">
+                  <polygon points="8.4 11.79 8.4 16.14 8.4 19.08 9 19.08 11.15 19.08 11.87 19.08 11.87 11.03 8.4 11.03 8.4 11.79" />
+                  <polygon points="9.9 20.46 11.07 24.84 11.75 24.67 10.62 20.46 9.9 20.46" />
+                  <rect x="5.63" y="12.48" width="1.39" height="2.97" />
+                  <path d="M16,2A14,14,0,1,0,30,16,14,14,0,0,0,16,2Zm6.6,9.55,4.21-1.66a.69.69,0,0,1,.51,1.29l-4.21,1.66a.69.69,0,0,1-.51-1.29Zm5.15,3.13a.69.69,0,0,1-.69.69H23.69a.69.69,0,1,1,0-1.38h3.37A.69.69,0,0,1,27.75,14.67Zm-6.49,4.6a.69.69,0,0,1-.69.69h-.13l-7.18-1.42v1.24a.69.69,0,0,1-.69.69H12L13.27,25a.69.69,0,0,1-.5.85l-2,.51-.17,0a.69.69,0,0,1-.67-.51L8.47,20.46H7.71A.69.69,0,0,1,7,19.77V16.84H4.94a.69.69,0,0,1-.69-.69V11.79a.69.69,0,0,1,.69-.69H7v-.76a.69.69,0,0,1,.69-.69h4.85a.69.69,0,0,1,.69.69v1.24l7.18-1.42a.69.69,0,0,1,.82.68Zm6.44-.2a.69.69,0,0,1-.9.39L22.6,17.8a.69.69,0,0,1,.51-1.29l4.21,1.66A.69.69,0,0,1,27.7,19.07Z" />
+                  <polygon points="13.25 17.12 19.88 18.43 19.88 11.68 13.25 12.99 13.25 17.12" />
+                </g>
+              </svg>
+              <span>{post.text_content}</span>{" "}
+            </p>
+            {post.address ? (
+              <p className="address-p">
+                <svg
+                  width="35px"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 60 60"
+                >
+                  <defs>
+                    <style>
+                      {
+                        ".prefix__cls-2{fill:none;stroke:#6b400d;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px}"
+                      }
+                    </style>
+                  </defs>
+                  <title>{"Location"}</title>
+                  <g id="prefix__\u56FE\u5C42_2" data-name="\u56FE\u5C42 2">
+                    <g id="prefix__Icons">
+                      <g id="prefix__Location">
+                        <path fill="none" d="M0 0h60v60H0z" />
+                        <path
+                          className="prefix__cls-2"
+                          d="M30 46.67S17.5 32.74 17.5 25.83a12.5 12.5 0 0125 0c0 3.43-3.08 8.6-6.19 12.92"
+                        />
+                        <path
+                          fill="#f2f0e7"
+                          stroke="#6b400d"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M22.8 46.67h14.4"
+                        />
+                        <path
+                          d="M30 17.5a8.34 8.34 0 108.33 8.33A8.33 8.33 0 0030 17.5zm0 12.18a3.85 3.85 0 113.85-3.85A3.85 3.85 0 0130 29.68z"
+                          fill="#ffc824"
+                        />
+                        <path
+                          className="prefix__cls-2"
+                          d="M30 22a3.85 3.85 0 11-3.85 3.84"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+                <span>{post.address}</span>{" "}
+              </p>
+            ) : null}
+
             <div className="likes">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90 90">
                 <title>{"carrot"}</title>
