fec64e89219028eee580ca9fa4b520b7805314cf
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-08-19 10:57:28 +0100

    profile changes

--- frontend/food-front-end/src/pages/profile.tsx
+++ frontend/food-front-end/src/pages/profile.tsx
@@ -6,35 +6,33 @@ import useStore from "../store";
 function Profile() {
   return (
     <main className="profile-page">
+      <h2 className="edit-profile-title">Your profile:</h2>
+
       <div className="profile-to-edit">
-        <h2 className="edit-profile-title">Your profile:</h2>
         <div className="edit-avatar">
           <img
-            className="avatar"
+            className="edit-avatar"
             src="https://www.adorama.com/alc/wp-content/uploads/2017/06/1-shutterstock_588634790.jpg"
             alt="avatar"
           ></img>
-          <button className="edit-profile">Edit</button>
         </div>
         <div className="edit-element">
           <p>Username</p>
-          <button className="edit-profile">Edit</button>
         </div>
         <div className="edit-element">
           <p>firstName</p>
-          <button className="edit-profile">Edit</button>
         </div>
 
         <div className="edit-element">
           <p>lastName</p>
-          <button className="edit-profile">Edit</button>
         </div>
 
         <div className="edit-element">
           <p>email</p>
+        </div>
+        <div className="edit-profile-buttons">
           <button className="edit-profile">Edit</button>
         </div>
-        <button className="edit-profile-submit">Submit</button>
       </div>
     </main>
   );
