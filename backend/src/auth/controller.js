ba2101bc2bc93a3bf001bfc6536b2e269f8d014b
Linlin Li <paris1900lll@gmail.com>
2021-08-25 22:35:28 +0100

    changes

--- backend/src/auth/controller.js
+++ backend/src/auth/controller.js
@@ -4,7 +4,14 @@ async function validateUser(req, res) {
   console.log("line 4", req.body);
   try {
     const result = await getModifiedUserInfo(req.body);
-    res.json({ id: result.id, username: result.username });
+    res.json({
+      id: result.id,
+      username: result.username,
+      first_name: result.first_name,
+      last_name: result.last_name,
+      email: result.email,
+      avatar: result.avatar,
+    });
   } catch (error) {
     res.status(401).json({ error: error.message });
   }
